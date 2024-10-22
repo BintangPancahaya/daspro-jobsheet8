cdcb50702e1bb90de3404fc1a4163f4a6c914b51
BintangPancahaya <bintang.panca007@gmail.com>
2024-10-22 17:48:42 +0700

    new siakad

--- RataNilai05.java
+++ RataNilai05.java
@@ -7,18 +7,23 @@ import java.util.Scanner;
         public static void main(String[] args) {
             Scanner input05 = new Scanner(System.in);
 
-            int i, j;
-            float nilai, totalNilai, rataNilai;
+            int i, j, nilai;
+            float totalNilai, rataNilai;
 
-            for (i=1;i<=5;i++) {
+            i=1;
+            while (i<=5) {
                 totalNilai = 0;
                 for (j=1;j<=5;j++) {
+                    if (i>=1 && i<= 5 && j==1) {
+                    System.out.println("Input Nilai Mahasiswa ke " + i);
+                    }
                     System.out.print("Nilai ke-" + j + " = ");
-                    nilai = input05.nextFloat();
+                    nilai = input05.nextInt();
                     totalNilai += nilai;
                 }
                 rataNilai = totalNilai/5;
-                System.out.print(rataNilai);
+                System.out.println("Rata-rata Nilai Mahasiswa ke " + i + " adalah  " + rataNilai);
+                i++;
             }
         }
     }
